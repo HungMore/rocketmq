689fac1f5f8005eb5e718971e33c5b79e74f7a93
mo <mo>
2024-05-20 16:55:41 +0800

    alg note

---
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/ProducerAlgBinaryTreeTest.java
@@ -0,0 +1,48 @@
+package org.apache.rocketmq.example.quickstart;
+
+/**
+ * @author mo
+ * @Description 生产者测试
+ * @createTime 2024年05月14日 10:07
+ */
+public class ProducerAlgBinaryTreeTest {
+
+    public static void main(String[] args) throws Exception {
+        ProducerAlgBinaryTreeTest test = new ProducerAlgBinaryTreeTest();
+        System.out.println();
+
+//        System.out.println(producerAlgTest.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+//        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+//        producer.setNamesrvAddr("127.0.0.1:9876");
+//        producer.start();
+//        Message message2 = new Message("order_topic",
+//                null,
+//                "key1",
+//                JSON.toJSONString("").getBytes(StandardCharsets.UTF_8));
+//        producer.send(message2, new SelectMessageQueueByHash(), "12");
+    }
+
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
+
+
