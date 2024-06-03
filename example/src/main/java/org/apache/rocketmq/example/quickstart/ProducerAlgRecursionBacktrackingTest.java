8befa9066b1365b06aa4fe875a5d87de43d12270
mo <mo>
2024-06-03 09:59:24 +0800

    alg note

---
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/ProducerAlgRecursionBacktrackingTest.java
@@ -0,0 +1,26 @@
+package org.apache.rocketmq.example.quickstart;
+
+/**
+ * @author mo
+ * @Description 生产者测试
+ * @createTime 2024年06月03日 09:58
+ */
+public class ProducerAlgRecursionBacktrackingTest {
+
+    public static void main(String[] args) throws Exception {
+        ProducerAlgRecursionBacktrackingTest test = new ProducerAlgRecursionBacktrackingTest();
+        System.out.println();
+
+//        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+//        producer.setNamesrvAddr("127.0.0.1:9876");
+//        producer.start();
+//        Message message2 = new Message("order_topic",
+//                null,
+//                "key1",
+//                JSON.toJSONString("").getBytes(StandardCharsets.UTF_8));
+//        producer.send(message2, new SelectMessageQueueByHash(), "12");
+
+
+    }
+
+}
