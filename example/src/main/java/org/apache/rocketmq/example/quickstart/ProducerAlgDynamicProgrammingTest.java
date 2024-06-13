8d8eae688c6060e1fea5c4ebaae46cf25ea8b04a
mo <mo>
2024-06-13 15:40:57 +0800

    alg 动态规划

---
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/ProducerAlgDynamicProgrammingTest.java
@@ -0,0 +1,63 @@
+package org.apache.rocketmq.example.quickstart;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author mo
+ * @Description 生产者测试
+ * @createTime 2024年06月03日 09:58
+ */
+@Slf4j
+public class ProducerAlgDynamicProgrammingTest {
+
+    public static void main(String[] args) throws Exception {
+        ProducerAlgDynamicProgrammingTest test = new ProducerAlgDynamicProgrammingTest();
+        System.out.println();
+
+//        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+//        producer.setNamesrvAddr("127.0.0.1:9876");
+//        producer.start();
+//        Message message2 = new Message("order_topic",
+//                null,
+//                "key1",
+//                JSON.toJSONString("").getBytes(StandardCharsets.UTF_8));
+//        producer.send(message2, new SelectMessageQueueByHash(), "12");
+
+        test.fib(5);
+        System.out.println(test.count);
+
+        test.count = 0;
+        test.fibMemo(5);
+        System.out.println(test.count);
+    }
+
+    // 计算执行fib函数的次数
+    int count = 0;
+
+    public int fib(int n) {
+        count++;
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    Map<Integer, Integer> memo = new HashMap<>();
+
+    public int fibMemo(int n) {
+        count++;
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        Integer res = memo.get(n);
+        if (res == null) {
+            res = fibMemo(n - 1) + fibMemo(n - 2);
+            memo.put(n, res);
+        }
+        return res;
+    }
+
+}
