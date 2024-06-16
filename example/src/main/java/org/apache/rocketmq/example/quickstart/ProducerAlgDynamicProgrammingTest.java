8354baa19cff4047870311e4c90825057e7ebb89
mo <mozh@vv.tech>
2024-06-16 11:10:38 +0800

    alg 爬楼梯

--- example/src/main/java/org/apache/rocketmq/example/quickstart/ProducerAlgDynamicProgrammingTest.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/ProducerAlgDynamicProgrammingTest.java
@@ -60,4 +60,14 @@ public class ProducerAlgDynamicProgrammingTest {
         return res;
     }
 
+    public int climbStairs(int n) {
+        int[] dp = new int[Math.max(n + 1, 3)];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
 }
