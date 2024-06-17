3ec1aeb65a6074532ec14ac8119078e8f48f1f03
mo <mo>
2024-06-17 14:39:12 +0800

    alg 完全平方数

--- example/src/main/java/org/apache/rocketmq/example/quickstart/ProducerAlgDynamicProgrammingTest.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/ProducerAlgDynamicProgrammingTest.java
@@ -144,4 +144,16 @@ public class ProducerAlgDynamicProgrammingTest {
         return dp[n];
     }
 
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int res = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                res = Math.min(res, 1 + dp[i - j * j]);
+            }
+            dp[i] = res;
+        }
+        return dp[n];
+    }
+
 }
