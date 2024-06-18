6ffc18a6a6f3d83522e3d8f532101816b90454dd
mo <mozh@vv.tech>
2024-06-18 23:01:26 +0800

    alg 打家劫舍

--- example/src/main/java/org/apache/rocketmq/example/quickstart/ProducerAlgDynamicProgrammingTest.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/ProducerAlgDynamicProgrammingTest.java
@@ -200,4 +200,17 @@ public class ProducerAlgDynamicProgrammingTest {
         return dp[m - 1][n - 1];
     }
 
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 2; i <= nums.length; i++) {
+            dp[i] = Math.max(nums[i - 1] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[nums.length];
+    }
+
 }
