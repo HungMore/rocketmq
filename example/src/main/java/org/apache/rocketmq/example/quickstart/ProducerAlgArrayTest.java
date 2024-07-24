b14806131e9d11501767591243b3a5d13cd6acdb
mo <mozh@vv.tech>
2024-07-24 16:05:43 +0800

    alg 寻找两个正序数组的中位数

---
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/ProducerAlgArrayTest.java
@@ -0,0 +1,49 @@
+package org.apache.rocketmq.example.quickstart;
+
+/**
+ * @author mo
+ * @Description 生产者测试
+ * @createTime 2024年05月14日 10:07
+ */
+public class ProducerAlgArrayTest {
+
+    public static void main(String[] args) throws Exception {
+        ProducerAlgArrayTest test = new ProducerAlgArrayTest();
+
+//        System.out.println(producerAlgTest.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+//        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+//        producer.setNamesrvAddr("127.0.0.1:9876");
+//        producer.start();
+//        Message message2 = new Message("order_topic",
+//                null,
+//                "key1",
+//                JSON.toJSONString("").getBytes(StandardCharsets.UTF_8));
+//        producer.send(message2, new SelectMessageQueueByHash(), "12");
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] merge = new int[nums1.length + nums2.length];
+        int index = 0, cursor1 = 0, cursor2 = 0;
+        while (cursor1 < nums1.length && cursor2 < nums2.length) {
+            if (nums1[cursor1] <= nums2[cursor2]) {
+                merge[index++] = nums1[cursor1++];
+            } else {
+                merge[index++] = nums2[cursor2++];
+            }
+        }
+        while (cursor1 < nums1.length) {
+            merge[index++] = nums1[cursor1++];
+        }
+        while (cursor2 < nums2.length) {
+            merge[index++] = nums2[cursor2++];
+        }
+        if (merge.length % 2 == 0) {
+            int mid = merge.length / 2;
+            return ((double) (merge[mid] + merge[mid - 1])) / 2;
+        } else {
+            return merge[merge.length / 2];
+        }
+    }
+
+
+}
